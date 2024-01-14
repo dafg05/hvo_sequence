e53279ad407ed3512d4b2b5712ab9e71147cba8e
dafg05 <danialefloresg@gmail.com>
2024-01-13 19:58:12 -0500

    Basic setup.py (missing license and other info)

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hvo_sequence',
+    version='0.1.0',
+    author='Behzad Haki, Marina Nieto, Teresa Pelinski',
+    description="A Piano Roll Representation for Drums Similar to Sequence Representations in Magenta's GrooVAE",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dafg05/hvo_sequence',
+    packages=find_packages(),
+    install_requires=[
+        'note_seq',
+        'pretty_midi',
+        'bokeh',
+        'soundfile',
+        'librosa',
+        'matplotlib',
+        'scipy',
+        'numpy'
+    ],
+    classifiers=[
+        # Choose classifiers as appropriate for your project
+        'Intended Audience :: Developers',
+    ],
+    python_requires='>=3.6',  # Specify compatible Python versions
+)
